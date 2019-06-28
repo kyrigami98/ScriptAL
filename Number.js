4876105e41ac52a77ab445add1b51d3a352a42b7
Unknown <Kyrigami@gmail.com>
2019-06-28 17:24:36 +0200

    refracto

--- Number.js
+++ Number.js
@@ -1,4 +1,22 @@
-var Number = function (number) {
+var Number = function (countrycode, values) {
 
+  var _countrycode = countrycode;
+  var _values = values;
+  
+  this.setCountrycode = function (countrycode) {
+    _countrycode = countrycode;
+  };
+  
+  this.getCountrycode = function () {
+   return _countrycode;
+  };
+
+  this.getValues = function () {
+   return _values;
+  };
+  
+  this.setValues = function (number) {
+   _values = number;
+  };
 
 };
